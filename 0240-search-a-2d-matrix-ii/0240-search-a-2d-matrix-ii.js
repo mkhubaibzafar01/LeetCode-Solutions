d944e37f5d4e68abc45a1173ad4e6d0f8ce2eff8
M. Khubaib Zafar <m.khubaib.zafar01@gmail.com>
2026-09-23 07:44:39 +0500

    Time: 242 ms (97.57%), Space: 59 MB (22.97%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,14 @@
+var searchMatrix = function(matrix, target) {
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while (row < matrix.length && col >= 0) {
+        if (matrix[row][col] === target) {
+            return true;
+        } else if (matrix[row][col] > target) {
+            col--;
+        } else {
+            row++;
+        }
+    }
+    return false;
+};
