966f9aeb95a5374d063838fee1d5723cc7a0aac3
M. Khubaib Zafar <m.khubaib.zafar01@gmail.com>
2026-09-24 09:58:33 +0500

    Time: 4 ms (51.47%), Space: 55.4 MB (58.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,19 @@
+var isValid = function(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    for (let char of s) {
+        if (char in map) {
+            const topElement = stack.length === 0 ? '#' : stack.pop();
+            if (topElement !== map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+};
