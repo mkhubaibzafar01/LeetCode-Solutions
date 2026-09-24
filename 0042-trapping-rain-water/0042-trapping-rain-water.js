d72cecfd2959b710710c9514bdb626758dc289b2
M. Khubaib Zafar <m.khubaib.zafar01@gmail.com>
2026-09-24 10:06:38 +0500

    Time: 0 ms (100%), Space: 55.2 MB (62.98%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,23 @@
+var trap = function(height) {
+    if (!height || height.length === 0) return 0;
+    
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = height[left];
+    let rightMax = height[right];
+    let water = 0;
+    
+    while (left < right) {
+        if (leftMax < rightMax) {
+            left++;
+            leftMax = Math.max(leftMax, height[left]);
+            water += leftMax - height[left];
+        } else {
+            right--;
+            rightMax = Math.max(rightMax, height[right]);
+            water += rightMax - height[right];
+        }
+    }
+    
+    return water;
+};
