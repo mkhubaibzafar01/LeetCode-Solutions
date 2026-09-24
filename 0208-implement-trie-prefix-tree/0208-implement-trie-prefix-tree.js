c442073f79bcaa0e5f96389a4a9e1a0ff6539dc7
M. Khubaib Zafar <m.khubaib.zafar01@gmail.com>
2026-09-24 10:07:29 +0500

    Time: 43 ms (60.03%), Space: 73.7 MB (80.58%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
@@ -0,0 +1,60 @@
+class TrieNode {
+    constructor() {
+        this.children = {}; // Maps characters to child TrieNodes
+        this.isWordEnd = false; // True if a word ends at this node
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /** 
+     * Inserts a word into the trie.
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {
+        let node = this.root;
+        for (const char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isWordEnd = true;
+    }
+
+    /** 
+     * Returns true if the word is in the trie.
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {
+        let node = this.root;
+        for (const char of word) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return node.isWordEnd;
+    }
+
+    /** 
+     * Returns true if there is any word in the trie that starts with the given prefix.
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {
+        let node = this.root;
+        for (const char of prefix) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return true;
+    }
+}
